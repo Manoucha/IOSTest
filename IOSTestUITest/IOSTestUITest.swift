0fe16d2d27b2c6eea66ad9fee932c54ea464433f
Imene <imene.ghodhbeni@esprit.tn>
2023-04-09 16:00:33 +0100

    Cleaning code and Adding UITests

--- IOSTestUITest/IOSTestUITest.swift
+++ IOSTestUITest/IOSTestUITest.swift
@@ -18,6 +18,7 @@ final class IOSTestUITest: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
+    
     ///  Test that the Scrollview is updated correctly in all views when the button is clicked
     
     func testScrollViewUpdatedWhenButtonClicked() throws {
